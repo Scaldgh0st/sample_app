bea62d8f605325de8e2e271a656c7bf62c426033
Ethereal-Acid <ethereal@acidfactory.ca>
2018-09-08 01:42:59 -0600

    Add Title Helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,13 +8,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_setup}"
+    assert_select "title", @base_setup
   end
   
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_setup}"
+    assert_select "title", @base_setup
   end
 
   test "should get help" do
